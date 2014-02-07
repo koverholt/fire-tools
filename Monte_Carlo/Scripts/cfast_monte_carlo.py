2e51c3eab51b4da60ed91a0fd08a012e7f1e4704
Kristopher Overholt <koverholt@gmail.com>
2014-02-07 07:51:13 +0000

    Monte Carlo: Add three cases: model bias/uncert.; input uncert.; combined uncert.

--- Monte_Carlo/Scripts/cfast_monte_carlo.py
+++ Monte_Carlo/Scripts/cfast_monte_carlo.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
-"""Run CFAST Monte Carlo Simulation"""
+"""
+Run CFAST Monte Carlo Simulation
+
+Case 1: Only model bias/uncertainty
+Case 2: Only input uncertainty
+Case 3: Combined model bias/uncertainty and input uncertainty
+"""
 
 import numpy as np
 from pylab import *
+from scipy.stats import norm
 import external_cfast
 
 np.set_printoptions(precision=0)
@@ -12,30 +19,30 @@ np.set_printoptions(precision=0)
 #  = Monte Carlo parameters =
 #  ==========================
 
-mc_iterations = 20
+mc_iterations = 10000
 
-#  =====================
-#  = Varied parameters =
-#  =====================
+# HRR, kW
+hrr = 500
 
-# Fixed value          - np.repeat(value, size)
-# Uniform distribution - np.random.uniform(lower, upper, size)
-# Normal distribution  - np.random.normal(mean, std, size)
+# +/- percent to vary HRR
+variation = 0.20
 
-# x, y, z compartment dimensions, m
-x = np.repeat(26.5, mc_iterations)
-y = np.repeat(18.5, mc_iterations)
-z = np.repeat(6.1, mc_iterations)
+#  ====================
+#  = Plotting options =
+#  ====================
 
-# Gas phase heat of combustion, kJ/kg
-hoc = np.repeat(20900, mc_iterations)
+histogram_bins = 50
 
-# Fire size, kW
-hrr = np.random.uniform(400, 600, mc_iterations)
+y_pdf_upper = 0.05
 
 #  ====================
 #  = Fixed parameters =
 #  ====================
+
+# x, y, z compartment dimensions, m
+x = 26.5
+y = 18.5
+z = 6.1
 
 # Ambient temperature, C
 tmp_a = 20
@@ -43,6 +50,9 @@ tmp_a = 20
 # Simulation time and data dump frequency, s
 simulation_time = 3600
 dt_data = 10
+
+# Gas phase heat of combustion, kJ/kg
+hoc = 20900
 
 # Wall material
 wall = 'concrete'
@@ -56,12 +66,26 @@ wall = 'concrete'
 delta = 1.15
 sigma_m = 0.20
 
+#  =====================
+#  = Varied parameters =
+#  =====================
+
+# Fixed value          - np.repeat(value, size)
+# Uniform distribution - np.random.uniform(lower, upper, size)
+# Normal distribution  - np.random.normal(mean, std, size)
+
+hrr_lower = hrr - (hrr * variation)
+hrr_upper = hrr + (hrr * variation)
+
+hrr_point = hrr
+hrr_uniform = np.random.uniform(hrr_lower, hrr_upper, mc_iterations)
+
 #  ============================
 #  = Plot input distributions =
 #  ============================
 
 figure()
-hist(hrr, bins=20, normed=1)
+hist(hrr_uniform, bins=histogram_bins, normed=1)
 xlabel('HRR (kW)', fontsize=20)
 ylabel('PDF', fontsize=20)
 grid(True)
@@ -71,10 +95,10 @@ for xlabel_i in ax.get_xticklabels():
 for ylabel_i in ax.get_yticklabels():
     ylabel_i.set_fontsize(16)
 gcf().subplots_adjust(left=0.15, bottom=0.11)
-savefig('../Figures/input_hrr_PDF.pdf')
+savefig('../Figures/input_PDF.pdf')
 
 figure()
-hist(hrr, bins=20, normed=1, histtype='step', cumulative=True)
+hist(hrr_uniform, bins=histogram_bins, normed=1, histtype='step', cumulative=True)
 xlabel('HRR (kW)', fontsize=20)
 ylabel('CDF', fontsize=20)
 ylim([0, 1])
@@ -85,22 +109,18 @@ for xlabel_i in ax.get_xticklabels():
 for ylabel_i in ax.get_yticklabels():
     ylabel_i.set_fontsize(16)
 gcf().subplots_adjust(left=0.15, bottom=0.11)
-savefig('../Figures/input_hrr_CDF.pdf')
+savefig('../Figures/input_CDF.pdf')
 
 #  ==========================
 #  = Monte Carlo simulation =
 #  ==========================
 
-output_hgl_temps = np.array([])
-
-for i in range(mc_iterations):
-
-    # Time and HRR ramp, s, kW
-    time_ramp = np.array([0, simulation_time])
-    hrr_ramp = np.array([hrr[i], hrr[i]])
-    
-    hgl_temp = external_cfast.run_case(x=x[i], y=y[i], z=z[i],
-                                       hoc=hoc[i],
+# Case 1 - Only model bias/uncertainty
+# Time and HRR ramp, s, kW
+time_ramp = np.array([0, simulation_time])
+hrr_ramp = np.array([hrr, hrr])
+hgl_temp_point = external_cfast.run_case(x=x, y=y, z=z,
+                                       hoc=hoc,
                                        tmp_a=tmp_a,
                                        time_ramp=time_ramp,
                                        hrr_ramp=hrr_ramp,
@@ -108,43 +128,138 @@ for i in range(mc_iterations):
                                        simulation_time=simulation_time,
                                        dt_data=dt_data)
 
+# Adjust for model bias and uncertainty
+hgl_temp_rise = (hgl_temp_point - tmp_a)
+mu_point = tmp_a + hgl_temp_rise / delta
+sigma_point = sigma_m * hgl_temp_rise / delta
+
+# Case 2 - Only input uncertainty
+# Case 3 - Combined model bias/uncertainty and input uncertainty
+output_hgl_temps = np.array([])
+output_hgl_temps_adjusted = np.array([])
+
+for i in range(mc_iterations):
+    # Time and HRR ramp, s, kW
+    time_ramp = np.array([0, simulation_time])
+    hrr_ramp = np.array([hrr_uniform[i], hrr_uniform[i]])
+    
+    hgl_temp = external_cfast.run_case(x=x, y=y, z=z,
+                                       hoc=hoc,
+                                       tmp_a=tmp_a,
+                                       time_ramp=time_ramp,
+                                       hrr_ramp=hrr_ramp,
+                                       wall=wall,
+                                       simulation_time=simulation_time,
+                                       dt_data=dt_data)
+
+    # Case 2 - only input uncertainty
+    output_hgl_temps = np.append(hgl_temp, output_hgl_temps)
+
     # Adjust for model bias and uncertainty
     hgl_temp_rise = (hgl_temp - tmp_a)
     mu_star = tmp_a + hgl_temp_rise / delta
     sigma_star = sigma_m * hgl_temp_rise / delta
-
-    output_hgl_temps = np.append(hgl_temp, output_hgl_temps)
+    hgl_temp_adjusted = np.random.normal(mu_star, sigma_star)
+    output_hgl_temps_adjusted = np.append(hgl_temp_adjusted,
+                                          output_hgl_temps_adjusted)
 
 #  =============================
 #  = Plot output distributions =
 #  =============================
 
+lower = mu_point - 4*sigma_point
+upper = mu_point + 4*sigma_point
+range = np.arange(lower , upper, 0.001)
+
 figure()
-hist(output_hgl_temps, bins=20, normed=1)
+plot(range, norm.pdf(range, mu_point, sigma_point))
 xlabel(r'HGL Temperature ($^\circ$C)', fontsize=20)
 ylabel('PDF', fontsize=20)
 grid(True)
+xlim([lower, upper])
+ylim([0, y_pdf_upper])
 ax = gca()
 for xlabel_i in ax.get_xticklabels():
     xlabel_i.set_fontsize(16)
 for ylabel_i in ax.get_yticklabels():
     ylabel_i.set_fontsize(16)
 gcf().subplots_adjust(left=0.15, bottom=0.11)
-savefig('../Figures/output_hgl_temps_PDF.pdf')
+savefig('../Figures/output_PDF_1_model.pdf')
 
 figure()
-hist(output_hgl_temps, bins=20, normed=1, histtype='step', cumulative=True)
+hist(output_hgl_temps, bins=histogram_bins, normed=1)
+xlabel(r'HGL Temperature ($^\circ$C)', fontsize=20)
+ylabel('PDF', fontsize=20)
+grid(True)
+xlim([lower, upper])
+ax = gca()
+for xlabel_i in ax.get_xticklabels():
+    xlabel_i.set_fontsize(16)
+for ylabel_i in ax.get_yticklabels():
+    ylabel_i.set_fontsize(16)
+gcf().subplots_adjust(left=0.15, bottom=0.11)
+savefig('../Figures/output_PDF_2_input.pdf')
+
+figure()
+hist(output_hgl_temps_adjusted, bins=histogram_bins, normed=1)
+xlabel(r'HGL Temperature ($^\circ$C)', fontsize=20)
+ylabel('PDF', fontsize=20)
+grid(True)
+xlim([lower, upper])
+ylim([0, y_pdf_upper])
+ax = gca()
+for xlabel_i in ax.get_xticklabels():
+    xlabel_i.set_fontsize(16)
+for ylabel_i in ax.get_yticklabels():
+    ylabel_i.set_fontsize(16)
+gcf().subplots_adjust(left=0.15, bottom=0.11)
+savefig('../Figures/output_PDF_3_combined.pdf')
+
+figure()
+plot(range, norm.cdf(range, mu_point, sigma_point))
 xlabel(r'HGL Temperature ($^\circ$C)', fontsize=20)
 ylabel('CDF', fontsize=20)
 ylim([0, 1])
 grid(True)
+xlim([lower, upper])
 ax = gca()
 for xlabel_i in ax.get_xticklabels():
     xlabel_i.set_fontsize(16)
 for ylabel_i in ax.get_yticklabels():
     ylabel_i.set_fontsize(16)
 gcf().subplots_adjust(left=0.15, bottom=0.11)
-savefig('../Figures/output_hgl_temps_CDF.pdf')
+savefig('../Figures/output_CDF_1_model.pdf')
+
+figure()
+hist(output_hgl_temps, bins=histogram_bins, normed=1, histtype='step', cumulative=True)
+xlabel(r'HGL Temperature ($^\circ$C)', fontsize=20)
+ylabel('CDF', fontsize=20)
+ylim([0, 1])
+grid(True)
+xlim([lower, upper])
+ax = gca()
+for xlabel_i in ax.get_xticklabels():
+    xlabel_i.set_fontsize(16)
+for ylabel_i in ax.get_yticklabels():
+    ylabel_i.set_fontsize(16)
+gcf().subplots_adjust(left=0.15, bottom=0.11)
+savefig('../Figures/output_CDF_2_input.pdf')
+
+figure()
+hist(output_hgl_temps_adjusted, bins=histogram_bins, normed=1, histtype='step',
+     cumulative=True)
+xlabel(r'HGL Temperature ($^\circ$C)', fontsize=20)
+ylabel('CDF', fontsize=20)
+ylim([0, 1])
+grid(True)
+xlim([lower, upper])
+ax = gca()
+for xlabel_i in ax.get_xticklabels():
+    xlabel_i.set_fontsize(16)
+for ylabel_i in ax.get_yticklabels():
+    ylabel_i.set_fontsize(16)
+gcf().subplots_adjust(left=0.15, bottom=0.11)
+savefig('../Figures/output_CDF_3_combined.pdf')
 
 #  =================
 #  = Print results =
@@ -153,6 +268,16 @@ savefig('../Figures/output_hgl_temps_CDF.pdf')
 print 'HGL Temperatures:'
 print output_hgl_temps
 print
+print 'HGL Temperatures (Adjusted):'
+print output_hgl_temps_adjusted
+print
 print 'Minimum, Median, Mean, and Maximum HGL Temperatures:'
-print np.array([np.min(output_hgl_temps), np.median(output_hgl_temps), np.mean(output_hgl_temps), np.max(output_hgl_temps)])
+print np.array([np.min(output_hgl_temps), np.median(output_hgl_temps),
+                np.mean(output_hgl_temps), np.max(output_hgl_temps)])
+
+print 'Adjusted Minimum, Median, Mean, and Maximum HGL Temperatures:'
+print np.array([np.min(output_hgl_temps_adjusted),
+                np.median(output_hgl_temps_adjusted),
+                np.mean(output_hgl_temps_adjusted),
+                np.max(output_hgl_temps_adjusted)])
 
