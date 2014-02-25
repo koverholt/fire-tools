7a998b2c53bf548bc6e555037af648ab578ca6e7
Kristopher Overholt <koverholt@gmail.com>
2014-02-25 18:45:24 +0000

    Monte Carlo: Minor fix to comment

--- Monte_Carlo/Scripts/cfast_monte_carlo.py
+++ Monte_Carlo/Scripts/cfast_monte_carlo.py
@@ -137,7 +137,8 @@ for i in range(mc_iterations):
 
     # Case 3 - Adjust for model bias and uncertainty
     hgl_temp_rise = (hgl_temp - tmp_a)
-    # Set zero or negative temperature rise to small number to avoid problems
+    # If hgl_temp_rise is zero or negative,
+    # then set it to a small number to avoid problems
     if hgl_temp_rise <= 0:
       hgl_temp_rise = 0.0001
     mu_star = (hgl_temp_rise / delta) + tmp_a
